18149046b388a67a56cde20e72ec48de620c3458
a.rivera <a.riveraa99@gmail.com>
2024-04-22 00:03:42 -0500

    Added subscription duration event listener

--- js/script.js
+++ js/script.js
@@ -8,5 +8,11 @@ var subDuration = 1;
 subTypeElement.addEventListener("change", function(e){
     subType = e.target.value;
     // testing for the value of the type of subscription
-    console.log(subType);
+    //console.log(subType);
 });
+
+subDurationElement.addEventListener("change", function (e) {
+    subDuration = Number(e.target.value);
+    // this number comes from the OPTION VALUE in the html
+    console.log(subDuration);
+})
