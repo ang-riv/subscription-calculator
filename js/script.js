711f651ba69663913fbdd5b50e1594ea20bd5b0a
a.rivera <a.riveraa99@gmail.com>
2024-04-21 23:59:06 -0500

    Added event listener for the subscription type

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
 
+subTypeElement.addEventListener("change", function(e){
+    subType = e.target.value;
+    // testing for the value of the type of subscription
+    console.log(subType);
+});
